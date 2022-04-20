fedaf660c05d2befa2f0a3e4359c6ea6d9886cda
Lakeram Narine <nar19002@byui.edu>
2022-04-20 10:41:11 -0600

    Change name of function

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
-displayName = (req, res) => {
+const displayName = (req, res) => {
     const data = 'Ana Cardenas';
     res.status(200).send(data);
-}
+};
 
-displayHello = (req, res) => {
-    const data = 'Hello';
-    res.status(200).send(data);
+const displayHello = (req, res) => {
+    const hello = 'Hello';
+    res.status(200).send(hello);
 }
 
 
